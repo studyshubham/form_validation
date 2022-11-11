a00ab71ad4ec091878354f9af5e6dca3c633aa27
studyshubham <studyshubham007@gmail.com>
2022-11-11 14:21:58 +0530

    Added Regular Expressions in Register.js

--- src/Register.js
+++ src/Register.js
@@ -1,3 +1,6 @@
 import { useRef, useState, useEffect } from 'react';
 import { faCheck, faTimes, faInfoCircle } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
+const USER_REGEX = /^[a-zA-Z][a-zA-Z0-9-_]{3,23}$/;
+const PWD_REGEX = /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%]).{8,24}$/;
