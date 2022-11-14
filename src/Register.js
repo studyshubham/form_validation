c5d0c31ed17c15d8caf64e10ec8c0a49a201de21
studyshubham <studyshubham007@gmail.com>
2022-11-14 08:45:16 +0530

    Created Register Component , Refs , States and useEffect methods

--- src/Register.js
+++ src/Register.js
@@ -1,6 +1,44 @@
-import { useRef, useState, useEffect } from 'react';
+import { useRef, useState, useEffect, useRef } from 'react';
 import { faCheck, faTimes, faInfoCircle } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
+import React from 'react'
+
 const USER_REGEX = /^[a-zA-Z][a-zA-Z0-9-_]{3,23}$/;
 const PWD_REGEX = /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%]).{8,24}$/;
+
+const Register = () => {
+    const useRef = useRef();
+    const errRef = useRef();
+
+    const [user, setUser] = useState('');
+    const [validName, setValidName] = useState(false);
+    const [userFocus, setUserFocus] = useState(false);
+
+    const [pwd, setPwd] = useState('');
+    const [validPwd, setValidPwd] = useState(false);
+    const [userPwd, setUserPwd] = useState(false);
+
+    const [matchPwd, setMatchPwd] = useState('');
+    const [validMatch, setValidMatch] = useState(false);
+    const [matchFocus, setMatchFocus] = useState(false);
+
+    useEffect(() => {
+        userRef.current.focus();
+    },[])
+
+    useEffect(() => {
+        const result = USER_REGEX.test(user);
+        console.log(result);
+        console.log(user);
+        setValidName(result);
+    },[user])
+    
+    return (
+        <div>
+
+        </div>
+    )
+}
+
+export default Register
